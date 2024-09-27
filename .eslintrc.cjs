b2ca0e43fea7752e51d3bbe473a9fba8e46fcaab
chenliu <2144541526@qq.com>
2024-09-27 17:37:35 +0800

    version0

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,25 @@
+// /**
+//  * @type {import("@types/eslint").Linter.BaseConfig}
+//  */
+// module.exports = {
+//   extends: ['@remix-run/eslint-config', 'plugin:hydrogen/recommended'],
+//   rules: {
+//     'hydrogen/prefer-image-component': 'off',
+//     'no-useless-escape': 'off',
+//     'no-case-declarations': 'off',
+//     'no-console': ['warn', {allow: ['warn', 'error']}],
+//   },
+// };
 /**
  * @type {import("@types/eslint").Linter.BaseConfig}
  */
 module.exports = {
   extends: ['@remix-run/eslint-config', 'plugin:hydrogen/recommended'],
+  plugins: ['import'], // 确保这里包含 import 插件
   rules: {
     'hydrogen/prefer-image-component': 'off',
     'no-useless-escape': 'off',
     'no-case-declarations': 'off',
-    'no-console': ['warn', {allow: ['warn', 'error']}],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
   },
 };
